e9f21fa252a3b3c4307c65473b032f20d7a22163
vimarta <91630858+vimarta@users.noreply.github.com>
2023-10-26 16:56:51 +0200

    Update Attack.java

--- src/Attack.java
+++ src/Attack.java
@@ -1,3 +1,32 @@
 public class Attack {
-    
+    private String name;
+	 private int power;
+	 private int precision;
+	 private int power_points;
+	 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getPrecision() {
+		return precision;
+	}
+	public void setPrecision(int precision) {
+		this.precision = precision;
+	}
+	public int getPower_points() {
+		return power_points;
+	}
+	public void setPower_points(int power_points) {
+		this.power_points = power_points;
+	}
+	 
 }
