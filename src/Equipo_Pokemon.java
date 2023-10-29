3bb553aae96f4bf6073d970617b73e110520ecc9
joferngome <55464546+joferngome@users.noreply.github.com>
2023-10-29 13:28:39 +0100

    Cambios_Random

---
+++ src/Equipo_Pokemon.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Equipo_Pokemon {
+
+    private ArrayList<Pokemon> equipo = new ArrayList<>();
+    private final int numPokemon = 6;
+
+    public Equipo_Pokemon(ArrayList<Pokemon> equipo) {
+        this.equipo = equipo;
+
+
+    }
+    
+}
