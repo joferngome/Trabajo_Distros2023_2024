cd3d19e1aecccd916fbcd4e21dbfebdbc98e9b5f
joferngome <joferngome@unirioja.es>
2023-11-02 16:54:13 +0100

    Cositas2

--- src/Combate_Servidor.java
+++ src/Combate_Servidor.java
@@ -21,11 +21,35 @@ public class Combate_Servidor {
                     ObjectOutputStream oos = new ObjectOutputStream(rival.getOutputStream());
                     ObjectInputStream ois = new ObjectInputStream(rival.getInputStream())){
 
+                    ArrayList<Pokemon> pok1 = new ArrayList<>();
+
+
+
+
                     ArrayList<Tipo> tipos = new ArrayList<Tipo>();
-                    tipos.add(Tipo.LUCHA);
+
+
+                    ArrayList<Attack> ataquesHitmonchan = new ArrayList<>();
+                    Attack a1 = new Attack("Patada giro",100,100,5);
+                    Attack a2 = new Attack("Roca afilada",80,100,5);
+                    Attack a3 = new Attack("Puño hielo",75,100,5);
+                    Attack a4 = new Attack("Puño trueno",75,100,5);
+                    ataquesHitmonchan.add(a1);
+                    ataquesHitmonchan.add(a2);
+                    ataquesHitmonchan.add(a3);
+                    ataquesHitmonchan.add(a4);
+
 
                     Pokemon Hitmonchan = new Pokemon("Hitmonchan",600,50,20,10000,
-                            tipos,)
+                            tipos, ataquesHitmonchan);
+
+                    pok1.add(Hitmonchan);
+
+                    Equipo_Pokemon eq1 = new Equipo_Pokemon(pok1);
+
+
+
+
 
                 }
             }
